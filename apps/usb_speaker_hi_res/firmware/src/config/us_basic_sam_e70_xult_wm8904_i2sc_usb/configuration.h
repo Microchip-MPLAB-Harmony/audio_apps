6c8c8a1a06495f13d0f79fee71ff4429cf1c0533
Chuck Lipari <chuck.lipari@microchip.com>
2021-09-03 14:44:51 -0700

    MH3-59469 usb_speaker_hi_res/us_hi_res_basic_sam_e70_xult_wm8904_i2sc_usb.X:  Regenerated with updated codec/i2s (audio stops prematurely) also updates to bass_boost pic32mzef_c2

--- apps/usb_speaker_hi_res/firmware/src/config/us_basic_sam_e70_xult_wm8904_i2sc_usb/configuration.h
+++ apps/usb_speaker_hi_res/firmware/src/config/us_basic_sam_e70_xult_wm8904_i2sc_usb/configuration.h
@@ -80,13 +80,13 @@ extern "C" {
 // *****************************************************************************
 // *****************************************************************************
 /* TIME System Service Configuration Options */
-#define SYS_TIME_INDEX_0                     0
-#define SYS_TIME_MAX_TIMERS                  5
-#define SYS_TIME_HW_COUNTER_WIDTH            16
-#define SYS_TIME_HW_COUNTER_PERIOD           65535U
-#define SYS_TIME_HW_COUNTER_HALF_PERIOD	     (SYS_TIME_HW_COUNTER_PERIOD>>1)
-#define SYS_TIME_CPU_CLOCK_FREQUENCY         240000000
-#define SYS_TIME_COMPARE_UPDATE_EXECUTION_CYCLES      (900)
+#define SYS_TIME_INDEX_0                            (0)
+#define SYS_TIME_MAX_TIMERS                         (5)
+#define SYS_TIME_HW_COUNTER_WIDTH                   (16)
+#define SYS_TIME_HW_COUNTER_PERIOD                  (65535U)
+#define SYS_TIME_HW_COUNTER_HALF_PERIOD             (SYS_TIME_HW_COUNTER_PERIOD>>1)
+#define SYS_TIME_CPU_CLOCK_FREQUENCY                (240000000)
+#define SYS_TIME_COMPARE_UPDATE_EXECUTION_CYCLES    (900)
 
 
 
@@ -102,15 +102,14 @@ extern "C" {
 #define DRV_I2C_CLOCK_SPEED_IDX0              400000
 
 /* I2S Driver Instance 0 Configuration Options */
-#define DRV_I2S_INDEX_0                       0
-#define DRV_I2S_CLIENTS_NUMBER_IDX0           1
-#define DRV_I2S_QUEUE_DEPTH_COMBINED          128
-#define DRV_I2S_QUEUE_SIZE_IDX0               128
-#define DRV_I2S_DATA_LENGTH_IDX0              32
-#define DRV_I2S_INT_SRC_IDX0                  I2SC1_IRQn
-#define DRV_I2S_XMIT_DMA_CH_IDX0              SYS_DMA_CHANNEL_3
-#define DRV_I2S_RCV_DMA_CH_IDX0               SYS_DMA_CHANNEL_0
-
+#define DRV_I2S_INDEX_0                0
+#define DRV_I2S_CLIENTS_NUMBER_IDX0    1
+#define DRV_I2S_QUEUE_DEPTH_COMBINED                 (2*128)
+#define DRV_I2S_QUEUE_SIZE_IDX0        128
+#define DRV_I2S_DATA_LENGTH_IDX0       32
+#define DRV_I2S_INT_SRC_IDX0           I2SC1_IRQn
+#define DRV_I2S_XMIT_DMA_CH_IDX0       SYS_DMA_CHANNEL_3
+#define DRV_I2S_RCV_DMA_CH_IDX0        SYS_DMA_CHANNEL_0
 
 /* I2C Driver Common Configuration Options */
 #define DRV_I2C_INSTANCES_NUMBER              1
@@ -124,7 +123,7 @@ extern "C" {
 #define DRV_WM8904_CLIENTS_NUMBER                           1
 #define DRV_WM8904_INSTANCES_NUMBER                         1
 
-#define DRV_WM8904_MASTER_MODE                              false
+#define DRV_WM8904_I2S_MASTER_MODE                          false
 #define DRV_WM8904_AUDIO_SAMPLING_RATE                      96000
 #define DRV_WM8904_VOLUME	                      	        200
 #define DRV_WM8904_AUDIO_DATA_FORMAT_MACRO             	    DATA_32_BIT_I2S
@@ -139,7 +138,7 @@ extern "C" {
 //Codec Driver Instance
 #define DRV_CODEC_INDEX_0                                   DRV_WM8904_INDEX_0
 #define sysObjdrvCodec0                                     sysObj.drvwm8904Codec0
-#define DRV_CODEC_I2S_MASTER_MODE                               DRV_WM8904_MASTER_MODE
+#define DRV_CODEC_I2S_MASTER_MODE                           DRV_WM8904_I2S_MASTER_MODE
 #define DRV_CODEC_BUFFER_HANDLE                             DRV_WM8904_BUFFER_HANDLE
 #define DRV_CODEC_BUFFER_HANDLE_INVALID                     DRV_WM8904_BUFFER_HANDLE_INVALID
 #define DRV_CODEC_BUFFER_EVENT_HANDLER                      DRV_WM8904_BUFFER_EVENT_HANDLER
@@ -154,6 +153,7 @@ extern "C" {
 #define DRV_CODEC_CHANNEL_RIGHT                             DRV_WM8904_CHANNEL_RIGHT
 #define DRV_CODEC_CHANNEL_LEFT_RIGHT                        DRV_WM8904_CHANNEL_LEFT_RIGHT
 #define DRV_CODEC_AUDIO_SAMPLING_RATE                       DRV_WM8904_AUDIO_SAMPLING_RATE
+#define DRV_CODEC_WHICH_MIC_INPUT                           DRV_WM8904_WHICH_MIC_INPUT
 #define DRV_CODEC_VOLUME	                        	    DRV_WM8904_VOLUME
 #define DRV_CODEC_AUDIO_DATA_FORMAT_MACRO                 	DRV_WM8904_AUDIO_DATA_FORMAT_MACRO
 #define DRV_CODEC_ENABLE_MIC_INPUT                          DRV_WM8904_ENABLE_MIC_INPUT
@@ -164,6 +164,7 @@ extern "C" {
 #define DRV_CODEC_Initialize                                DRV_WM8904_Initialize
 #define DRV_CODEC_Deinitialize                              DRV_WM8904_Deinitialize
 #define DRV_CODEC_Status                                    DRV_WM8904_Status
+#define DRV_CODEC_ClientReady                               DRV_WM8904_ClientReady
 #define DRV_CODEC_Tasks                                     DRV_WM8904_Tasks
 #define DRV_CODEC_Open                                      DRV_WM8904_Open
 #define DRV_CODEC_Close                                     DRV_WM8904_Close
